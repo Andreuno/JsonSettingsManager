ae337016dd4ea7b587d5cb2ad1f0f0462a3fb4d9
Andreuno <andreuno1234@gmail.com>
2022-02-14 03:58:05 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,5 +1,12 @@
 import json
 
+# For devoltage use:
+# config = Settings()
+#
+# Using your path to json file:
+# Settings.path_config = "config_file.json"
+# config = Settings()
+
 # =========================================================== CLASSES
 
 class Settings():
